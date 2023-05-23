9766f660159d7a561a99e64bed0e7738a04c4b97
FOAMY1 <111202080012@mhs.dinus.ac.id>
2023-05-23 23:40:50 +0700

    Tambah komponen kategori, asset preview, dan coil dependency

---
+++ app/src/main/java/com/mastour/mastour/ui/components/CategoryComponent.kt
@@ -0,0 +1,127 @@
+package com.mastour.mastour.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.mastour.mastour.R
+import com.mastour.mastour.ui.theme.MasTourTheme
+
+@Composable
+fun CategoryComponent(
+    name: String,
+    photoUrl: String,
+    modifier: Modifier = Modifier,
+    width: Int,
+    height: Int
+){
+    Card(modifier = modifier
+        .width((width).dp)
+        .height((height).dp)
+        .clip(RoundedCornerShape(8.dp))
+    ){
+        Box{
+            AsyncImage(
+                model = photoUrl,
+                contentDescription = name,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(8.dp)),
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colors.primary, blendMode = BlendMode.Screen)
+            )
+            Text(
+                text = name,
+                style = MaterialTheme.typography.subtitle2.copy(
+                    fontWeight = FontWeight.ExtraBold,
+                    shadow = Shadow(color = Color.Black,
+                        offset = Offset(4f, 4f),
+                        blurRadius = 8f)
+                ),
+                color = Color.White,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .offset(x = 4.dp, y = (-4).dp)
+            )
+        }
+    }
+}
+
+//CategoryComponent2 Drawable Preview Only (To Be Deleted)
+@Composable
+fun CategoryComponent2(
+    name: String,
+    photoUrl: Int,
+    modifier: Modifier = Modifier,
+    width: Int,
+    height: Int
+){
+    Card(modifier = modifier
+        .width((width).dp)
+        .height((height).dp)
+        .clip(RoundedCornerShape(8.dp))
+    ){
+        Box{
+            Image(
+                painter = painterResource(photoUrl),
+                contentDescription = name,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(8.dp)),
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colors.primary, blendMode = BlendMode.Softlight)
+            )
+            Text(
+                text = name,
+                style = MaterialTheme.typography.subtitle2.copy(
+                    fontWeight = FontWeight.ExtraBold,
+                    shadow = Shadow(color = Color.Black,
+                        offset = Offset(4f, 4f),
+                        blurRadius = 8f)
+                ),
+                color = Color.White,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .offset(x = 4.dp, y = (-4).dp)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryComponentPreview(){
+    MasTourTheme {
+        CategoryComponent2(name = "B Asylum", photoUrl = R.drawable.asylum, width = 120, height = 120)
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryComponentPreview2(){
+    MasTourTheme {
+        CategoryComponent(
+            name = "Monas",
+            photoUrl = "https://upload.wikimedia.org/wikipedia/id/thumb/b/b1/Merdeka_Square_Monas_02.jpg/800px-Merdeka_Square_Monas_02.jpg",
+            width = 120,
+            height = 120
+        )
+    }
+}
