2cdfaaa992263392c8b80e30437bada0a479fd8b
FOAMY1 <111202080012@mhs.dinus.ac.id>
2023-06-14 23:45:38 +0700

    Changed font to Barlow

--- app/src/main/java/com/mastour/mastour/ui/theme/Type.kt
+++ app/src/main/java/com/mastour/mastour/ui/theme/Type.kt
@@ -2,17 +2,26 @@ package com.mastour.mastour.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mastour.mastour.R
+
+val Barlow = FontFamily(
+    Font(R.font.barlow_regular),
+    Font(R.font.barlow_bold, FontWeight.Bold),
+    Font(R.font.barlow_extrabold, FontWeight.ExtraBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Barlow,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    defaultFontFamily = Barlow
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
